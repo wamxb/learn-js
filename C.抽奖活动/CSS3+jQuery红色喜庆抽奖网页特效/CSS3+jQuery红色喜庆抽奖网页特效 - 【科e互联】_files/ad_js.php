af5a1385eb4c5ab46826aa9d2fba34ff882b78ae
zplus <858184062@qq.com>
2015-12-04 11:10:20 +0800

    no message

--- C.抽奖活动/CSS3+jQuery红色喜庆抽奖网页特效/CSS3+jQuery红色喜庆抽奖网页特效 - 【科e互联】_files/ad_js.php
+++
@@ -1,3 +0,0 @@
-<!--
-document.write("<script type=\"text/javascript\">\r\n/*728*90，创建于2013-11-6*/\r\nvar cpro_id = \"u1404884\";\r\n</script>\r\n<script src=\"http://cpro.baidustatic.com/cpro/ui/c.js\" type=\"text/javascript\"></script>");
--->
