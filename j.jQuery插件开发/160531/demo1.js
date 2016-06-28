9fe78bc499e50e7abe6fb463bf1dfe95857d027e
zplus <858184062@qq.com>
2016-06-28 10:39:05 +0800

    no message

--- j.jQuery插件开发/160531/demo1.js
+++ j.jQuery插件开发/160531/demo1.js
@@ -1,3 +1,55 @@
-/**
- * Created by zplus on 16/5/31.
- */
+(function ($) {
+
+    var privateFunction  = function () {
+
+    };
+
+    var methods = {
+        init: function (options) {
+
+            return this.each(function () {
+                var $that = $(this);
+                console.log('init', options);
+
+                var defaults = {
+                    color: 'blue',
+                    size: 50
+                };
+                var settings = $.extend({}, defaults, options);
+                $that.css({
+                    margin: 10,
+                    width: settings.size,
+                    height: settings.size,
+                    backgroundColor: settings.color
+                })
+            });
+        },
+        destroy: function () {
+            return this.each(function () {
+               var $that = $(this);
+
+                $that.css({
+                    opacity: .5
+                })
+            });
+        }
+    };
+
+    $.fn.pluginName = function () {
+        var method = arguments[0];
+        var args;
+        if (methods[method]) { // 执行方法
+            method = methods[method];
+
+            args = Array.prototype.slice.call(arguments, 1);
+        } else if (typeof method == 'object' || !method) { // {} || 未传递参数
+            args = [method];
+            method = methods.init;
+        } else {
+            console.error('Method ' + method + ' does not exist on jQuery.pluginName');
+            return this;
+        }
+        return method.apply(this, args); // 方法执行中返回 this,最后能得到 this
+    }
+
+})(jQuery);
--- j.jQuery插件开发/160531/singleton.js
+++ j.jQuery插件开发/160531/singleton.js
@@ -1,3 +1,13 @@
-/**
- * Created by zplus on 16/5/31.
- */
+var singleton = function (fn) {
+    var result; // 寄存 div 的引用
+    return function () {
+        return result || (result = fn.apply(this, arguments));
+    }
+};
+
+// 桥接模式
+var createMask = singleton(function () {
+    return document.body.appendChild(document.createElement('div'));
+});
+
+// 策略模式 定义一系列的算法,把它们一个个封装起来,并且使它们可相互替换
